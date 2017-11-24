bd8e9c326990dcdf85a470a4aec96de8a3a58e02
hanwuji99 <hanwuji99@outlook.com>
2017-11-24 21:08:35 +0800

    add files

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env  python3
+from flask import Flask, render_template, url_for, redirect, abort, jsonify
+import os
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    path = '/home/hope/files/'
+    files = os.listdir(path)
+    titles = []
+    for file in files:
+        filename = path + file
+        with open(filename) as f:
+            json_dict = json.load(f)
+            title = json_dict['title']
+        titles.append(title)
+    return render_template('index.html', titles=titles)
+
+
+@app.route('/files/<filename>')
+def file(filename):
+    dirpath = '/home/hope/files/'
+    filepath = os.path.join(dirpath, filename)
+    if not os.path.exists(filepath):
+        abort(404)
+    else:
+        info = []
+        with open(filepath) as f:
+            json_dict = json.load(f)
+            print('jsdict', json_dict)
+            info.append(json_dict)
+        return render_template('file.html', info=info)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
