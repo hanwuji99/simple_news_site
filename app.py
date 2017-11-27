ff0600a6abcf078986a8535ff6da631966512a64
hanwuji <hanwuji99@outlook.com>
2017-11-27 21:33:09 +0800

    update

--- app.py
+++ app.py
@@ -1,44 +1,79 @@
-#!/usr/bin/env  python3
-from flask import Flask, render_template, url_for, redirect, abort, jsonify
-import os
-import json
+#!/usr/bin/env python
+# coding=utf-8
+from datetime import datetime
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 
+app.config.update(dict(
+    SQLALCHEMY_DATABASE_URI='mysql://root:@localhost/shiyanlou'
+))
+
+db = SQLAlchemy(app)
+
+
+class File(db.Model):
+    __tablename__ = 'files'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True)
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('Category', uselist=False)
+    content = db.Column(db.Text)
+
+    def __init__(self, title, created_time, category, content):
+        self.title = title
+        self.created_time = created_time
+        self.category = category
+        self.content = content
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    files = db.relationship('File')
+
+    def __init__(self, name):
+        self.name = name
+
+
+def insert_datas():
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+
+
 @app.route('/')
 def index():
-    path = '/home/shiyanlou/files/'
-    files = os.listdir(path)
-    titles = []
-    for file in files:
-        filename = path + file
-        with open(filename) as f:
-            json_dict = json.load(f)
-            title = json_dict['title']
-        titles.append(title)
-    return render_template('index.html', titles=titles)
+    return render_template('index.html', files=File.query.all())
 
 
-@app.route('/files/<filename>')
-def file(filename):
-    dirpath = '/home/shiyanlou/files/'
-    filepath = os.path.join(dirpath, filename + '.json')
-    if not os.path.exists(filepath):
-        abort(404)
-    else:
-        info = []
-        with open(filepath) as f:
-            json_dict = json.load(f)
-            print('jsdict', json_dict)
-            info.append(json_dict)
-        return render_template('file.html', info=info)
+@app.route('/files/<int:file_id>')
+def file(file_id):
+    file_item = File.query.get_or_404(file_id)
+    return render_template('file.html', file_item=file_item)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   # db.create_all()
+    #insert_datas()
+    #print('db')
+    app.run()
+    
